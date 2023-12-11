73937cc7f2114454f0471f1fce40b328e5358aad
Petrovskii <petrovskii@bpcbt.com>
2023-12-11 12:57:44 +0300

    Sprint#9 Pom file corrected again

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,6 +2,8 @@ package ru.yandex.practicum.filmorate.model;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.Builder;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.validators.BirthDate;
@@ -11,6 +13,7 @@ import java.time.LocalDate;
 @Data
 @Builder
 public class User {
+    @NotNull
     private int id;
 
     @Email
@@ -18,7 +21,7 @@ public class User {
     private String email;
     @NotBlank
     private String login;
-    @NotBlank
+
     private String name;
     @BirthDate
     private LocalDate birthday;
