e48081986f2d84c791ba9e5905e6fd68911eb5e8
Petrovskii <sergdest261276@yandex.ru>
2024-01-29 13:49:12 +0300

    Sprint#10 Correct checkstyle #2

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorResponse.java
@@ -6,6 +6,7 @@ import lombok.Data;
 public class ErrorResponse {
     private String error;
     private String description;
+
     public ErrorResponse(String error, String description) {
         this.error = error;
         this.description = description;
