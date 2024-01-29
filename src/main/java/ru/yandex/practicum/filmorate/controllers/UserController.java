8ab5effdca9d4afe18047ba90ad1388ab31901df
Petrovskii <sergdest261276@yandex.ru>
2024-01-29 13:47:18 +0300

    Sprint#10 Correct checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -25,58 +25,60 @@ public class UserController {
     }
 
     @GetMapping
-    public List<User> getAllUsers(){ //геттер для пользователей
+    public List<User> getAllUsers() { //геттер для пользователей
             return userService.getAllUsers();
     }
+
     @PostMapping
-    public User createUser(@Valid @RequestBody User user){ //создание пользователя
+    public User createUser(@Valid @RequestBody User user) { //создание пользователя
         return userService.createUser(user);
     }
 
     @PutMapping
-    public User updateUser(@Valid @RequestBody User user){ //обновление пользователя
+    public User updateUser(@Valid @RequestBody User user) { //обновление пользователя
         return userService.updateUser(user);
     }
 
     @PutMapping("/{id}/friends/{friendId}") //добавление в друзья
     public User userAddFriend(@PathVariable("id") final Integer userId,
-    @PathVariable("friendId") final Integer friendId){
+    @PathVariable("friendId") final Integer friendId) {
         checkIdUserAndFriend(userId, friendId);
         return userService.userAddFriend(userId, friendId);
     }
 
     @DeleteMapping("/{id}/friends/{friendId}") //удаление из друзей
-    public User userDeleteFriend ( @PathVariable("id") final Integer userId,
-    @PathVariable("friendId") final Integer friendId){
+    public User userDeleteFriend(@PathVariable("id") final Integer userId,
+    @PathVariable("friendId") final Integer friendId) {
         checkIdUserAndFriend(userId, friendId);
         return userService.userDeleteFriend(userId, friendId);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")//геттер для списка общих друзей
-    public List<User> getListFriend ( @PathVariable("id") final Integer userId,
-    @PathVariable("otherId") final Integer friendId){
+    public List<User> getListFriend(@PathVariable("id") final Integer userId,
+    @PathVariable("otherId") final Integer friendId) {
         checkIdUserAndFriend(userId, friendId);
         return userService.getFriends(userId, friendId);
     }
 
     @GetMapping("/{id}") //геттер по айди
-    public User getUserForId ( @PathVariable int id){
+    public User getUserForId(@PathVariable int id) {
         checkUserId(id);
         return userService.getUserForId(id);
     }
 
     @GetMapping("/{id}/friends")//получение списка друзей пользователя
-    public List<User> getFriendsUserForId (@PathVariable("id") Integer id){
+    public List<User> getFriendsUserForId(@PathVariable("id") Integer id) {
         checkUserId(id);
         return userService.getFriendsUserForId(id);
     }
 
-    private void checkUserId (Integer id){
+    private void checkUserId(Integer id) {
         if (id <= 0) {
             throw new IncorrectIDException("Параметр id пользователя имеет отрицательное значение.");
         }
     }
-    private void checkIdUserAndFriend (Integer idUser, Integer idFriend){
+
+    private void checkIdUserAndFriend(Integer idUser, Integer idFriend) {
         if (idUser <= 0) {
             throw new IncorrectIDException("Параметр id пользователя имеет отрицательное значение.");
         }
