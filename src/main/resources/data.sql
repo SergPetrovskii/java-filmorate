858b90f80a13bec4187d3901f3eeed68eae5be9e
Petrovskii <sergdest261276@yandex.ru>
2024-02-26 05:12:40 +0300

    Sprint#11 correct sql request #5

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,42 +1,42 @@
 merge into MPA
-    using values (1, 'G') as incoming(ID,NAME)
-    on MPA.ID = incoming.ID
+    using values (1, 'G') as incoming(MPA_ID,MPA_NAME)
+    on MPA.MPA_ID = incoming.MPA_ID
     when not matched then
-        insert (ID, NAME) values (incoming.ID, incoming.NAME)
+        insert (MPA_ID, MPA_NAME) values (incoming.MPA_ID, incoming.MPA_NAME)
     when matched then
-        update set NAME = incoming.NAME;
+        update set MPA_NAME = incoming.MPA_NAME;
 
 merge into MPA
-    using values (2, 'PG') as incoming(ID,NAME)
-    on MPA.ID = incoming.ID
+    using values (2, 'PG') as incoming(MPA_ID,MPA_NAME)
+    on MPA.MPA_ID = incoming.MPA_ID
     when not matched then
-        insert (ID, NAME) values (incoming.ID, incoming.NAME)
+        insert (MPA_ID, MPA_NAME) values (incoming.MPA_ID, incoming.MPA_NAME)
     when matched then
-        update set NAME = incoming.NAME;
+        update set MPA_NAME = incoming.MPA_NAME;
 
 merge into MPA
-    using values (3, 'PG-13') as incoming(ID,NAME)
-    on MPA.ID = incoming.ID
+    using values (3, 'PG-13') as incoming(MPA_ID,MPA_NAME)
+    on MPA.MPA_ID = incoming.MPA_ID
     when not matched then
-        insert (ID, NAME) values (incoming.ID, incoming.NAME)
+        insert (MPA_ID, MPA_NAME) values (incoming.MPA_ID, incoming.MPA_NAME)
     when matched then
-        update set NAME = incoming.NAME;
+        update set MPA_NAME = incoming.MPA_NAME;
 
 merge into MPA
-    using values (4, 'R') as incoming(ID,NAME)
-    on MPA.ID = incoming.ID
+    using values (4, 'R') as incoming(MPA_ID,MPA_NAME)
+    on MPA.MPA_ID = incoming.MPA_ID
     when not matched then
-        insert (ID, NAME) values (incoming.ID, incoming.NAME)
+        insert (MPA_ID, MPA_NAME) values (incoming.MPA_ID, incoming.MPA_NAME)
     when matched then
-        update set NAME = incoming.NAME;
+        update set MPA_NAME = incoming.MPA_NAME;
 
 merge into MPA
-    using values (5, 'NC-17') as incoming(ID,NAME)
-    on MPA.ID = incoming.ID
+    using values (5, 'NC-17') as incoming(MPA_ID,MPA_NAME)
+    on MPA.MPA_ID = incoming.MPA_ID
     when not matched then
-        insert (ID, NAME) values (incoming.ID, incoming.NAME)
+        insert (MPA_ID, MPA_NAME) values (incoming.MPA_ID, incoming.MPA_NAME)
     when matched then
-        update set NAME = incoming.NAME;
+        update set MPA_NAME = incoming.MPA_NAME;
 
 merge into GENRES
     using values (1, 'Комедия') as incoming(ID,NAME)
